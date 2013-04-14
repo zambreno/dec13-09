a8ce6f45e62ac2b1f56ff9c4e215374b8950d656
Brittany Oswald <brittany.oswald@gmail.com>
2013-04-14 03:38:35 -0500

    This will read MIDI input from the keyboard.

--- darren/src/readData.c
+++ darren/src/readData.c
@@ -2,10 +2,11 @@
 #include <portmidi.h>
 #include <porttime.h>
 
-int main() {
 
-	int cnt, i, dev; // dev is the device, should always be 3
-	PmError retVal;
+void readMIDI() {
+
+	int cnt, i, dev; // dev is the device, should always be 2
+	PmError retval;
 	const PmDeviceInfo *info;
 	PmEvent msg[32];
 	PortMidiStream *mstream;
@@ -20,10 +21,10 @@ int main() {
 				printf("%d: %s \n", i, info->name);
 			}
 		}
-		dev = 3;
+		dev = 2;
 		
-		Pt_Start(1, NULL, NULL, NULL);
-		retval = Pm_OpenInput(&msstream, dev, NULL, 512L, NULL, NULL);
+		Pt_Start(1, NULL, NULL);
+		retval = Pm_OpenInput(&mstream, dev, NULL, 512L, NULL, NULL);
 		
 		if(retval != pmNoError) {
 			printf("error: %s \n", Pm_GetErrorText(retval));
@@ -31,4 +32,28 @@ int main() {
 		else {
 			while(1) {
 				if(Pm_Poll(mstream)) {
-					cnt = Pm_Read(mstream
+					cnt = Pm_Read(mstream, msg, 32);
+					for(i=0; i<cnt; i++) {
+						printf("status:%d, byte1=%d, byte2=%d, time=%.3f\n",
+							Pm_MessageStatus(msg[i].message),
+							Pm_MessageData1(msg[i].message),
+							Pm_MessageData2(msg[i].message),
+							msg[i].timestamp/1000.0);
+					}
+				}
+			}
+		}
+		Pm_Close(mstream);
+	}
+	else {
+		printf("No MIDI devices found\n");
+	}
+	Pm_Terminate();
+	return;
+}
+
+
+int main() {
+	readMIDI();
+	return 0;
+}
